bdefb45d6a3ebfe8137ab81690004823c4f4c661
CharlotteXIII <sippanon2546@gmail.com>
2025-11-22 19:06:21 +0700

    Add Soldier Prefabs to Player_Code
    Finish Clip 2

--- FinalProject/Assets/Scripts/Soldier.cs
+++ FinalProject/Assets/Scripts/Soldier.cs
@@ -1,6 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Soldier : MonoBehaviour
+public class Soldier : Attack
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
